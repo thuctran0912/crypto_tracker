ff7687efab38b606de2a46e2f6dea2509c36ebbb
Thuc <huythuctran912@gmail.com>
2024-10-15 16:52:49 +0300

    updated on execution steps

--- snowpipe_streaming/scripts/execution/3_configure_environments.sh
+++ snowpipe_streaming/scripts/execution/3_configure_environments.sh
@@ -5,7 +5,3 @@ echo "export BS=$BS" >> ~/.bashrc
 ## Create Kafka Topic
 $HOME/snowpipe-streaming/kafka_2.12-2.8.1/bin/kafka-topics.sh --bootstrap-server $BS --command-config $HOME/snowpipe-streaming/scripts/client.properties --create --topic streaming --partitions 2 --replication-factor 2
 $HOME/snowpipe-streaming/kafka_2.12-2.8.1/bin/kafka-topics.sh --bootstrap-server $BS --command-config $HOME/snowpipe-streaming/scripts/client.properties --describe --topic streaming
-
-
-
-
