29273d7aac21fba3babb9fb9951b91666bfc61d7
Thuc <huythuctran912@gmail.com>
2024-10-17 18:39:27 +0300

    updated params

--- snowpipe_streaming/scripts/execution/3_configure_environments.sh
+++ snowpipe_streaming/scripts/execution/3_configure_environments.sh
@@ -3,5 +3,5 @@ export BS=[cluster_private_endpoint]
 echo "export BS=$BS" >> ~/.bashrc
 
 ## Create Kafka Topic
-$HOME/snowpipe-streaming/kafka_2.12-2.8.1/bin/kafka-topics.sh --bootstrap-server $BS --command-config $HOME/snowpipe-streaming/scripts/client.properties --create --topic streaming --partitions 2 --replication-factor 2
+$HOME/snowpipe-streaming/kafka_2.12-2.8.1/bin/kafka-topics.sh --bootstrap-server $BS --command-config $HOME/snowpipe-streaming/scripts/client.properties --create --topic trade_streaming --partitions 2 --replication-factor 2
 $HOME/snowpipe-streaming/kafka_2.12-2.8.1/bin/kafka-topics.sh --bootstrap-server $BS --command-config $HOME/snowpipe-streaming/scripts/client.properties --describe --topic streaming
--- snowpipe_streaming/scripts/execution/6_start_pipeline.sh
+++ snowpipe_streaming/scripts/execution/6_start_pipeline.sh
@@ -3,4 +3,4 @@ $HOME/snowpipe-streaming/kafka_2.12-2.8.1/bin/connect-standalone.sh $HOME/snowpi
 
 ## Start another session
 ## Start the producer that will ingest real-time data to the MSK cluster
-python3 $HOME/snowpipe-streaming/scripts/websocket_client/websocket_to_kafka.py | $HOME/snowpipe-streaming/kafka_2.12-2.8.1/bin/kafka-console-producer.sh --broker-list $BS --producer.config $HOME/snowpipe-streaming/scripts/client.properties --topic streaming
+python3 $HOME/snowpipe-streaming/scripts/websocket_client/websocket_to_kafka.py | $HOME/snowpipe-streaming/kafka_2.12-2.8.1/bin/kafka-console-producer.sh --broker-list $BS --producer.config $HOME/snowpipe-streaming/scripts/client.properties --topic trade_streaming
